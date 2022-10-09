c4cf3c64b6548e03659a5a46f549b4d048d49e7b
dineshreddy1marri <58055846+dineshreddy1marri@users.noreply.github.com>
2022-10-08 23:07:32 -0500

    Add files via upload

---
+++ src/main/java/com/javaGuidesYtd/springboot/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.javaGuidesYtd.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.javaGuidesYtd.springboot.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+
+}
